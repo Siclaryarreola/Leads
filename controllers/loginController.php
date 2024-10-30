6e03722acc79dc88bb9405503547ed840b2518cc
Siclary Arreola <Siclaryarreola04@gmail.com>
2024-10-30 14:44:46 -0600

    Update loginController.php
    Se modifica los roles

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -1,63 +1,55 @@
 <?php
-require_once('models/LoginModel.php'); // Ruta al documento de LoginModel
-require_once('SessionManager.php');    // Ruta al documento de SessionManager
+require_once('models/LoginModel.php');
+require_once('SessionManager.php');
 
-class LoginController 
-{
+class LoginController {
     private $loginModel;
 
-    public function __construct() 
-    {
+    public function __construct() {
         $this->loginModel = new LoginModel();
     }
 
-    public function showLoginForm() 
-    {
-        require('views/login.php'); // Ruta al login
+    public function showLoginForm() {
+        require('views/login.php');
     }
 
-    public function login() 
-    {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['email']) && !empty($_POST['password'])) 
-        {
-            $email = strtolower(trim($_POST['email']));
+    public function login() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['email']) && !empty($_POST['password'])) {
+            $email = strtolower(trim($_POST['email'])); // Normalizar email a minúsculas
             $password = $_POST['password'];
 
-            // Recuperar los datos del usuario desde el modelo de login
             $user = $this->loginModel->getUserByEmailAndPassword($email, $password);
-            if (is_array($user)) 
-            {
+            if (is_array($user)) {
                 SessionManager::createSession($user);
-
-                // Redirección basada en el rol del usuario
-                if ($user['rol'] == 1)
-                {
-                    header('Location: /Portal/views/admin/dashboardAdmin.php'); // Ruta al dashboard Admin
-                } 
-                else if ($user['rol'] == 2)
-                {
-                    header('Location: /Portal/views/gerente/dashboardGerente.php'); // Ruta al dashboard Gerente
-                }
-                else if ($user['rol'] == 0)
-                {
-                    header('Location: /Portal/views/user/dashboardUser.php'); // Ruta al dashboard User
-                }
-
-                exit;
-            } 
-            else if ($user === 'blocked')
-            {
+                $this->redirectUserBasedOnRole($user['rol']);
+            } else if ($user === 'blocked') {
                 $_SESSION['error'] = 'La cuenta está bloqueada temporalmente. Intente de nuevo más tarde.';
-                header('Location: index.php?action=showLoginForm'); // Ruta al Login
+                header('Location: index.php?action=showLoginForm');
                 exit;
-            } 
-            else 
-            {
+            } else {
                 $_SESSION['error'] = 'Credenciales inválidas.';
-                header('Location: index.php?action=showLoginForm'); // Ruta al login
+                header('Location: index.php?action=showLoginForm');
                 exit;
             }
         }
     }
+
+    private function redirectUserBasedOnRole($role) {
+        switch ($role) {
+            case 1:
+                header('Location: /Portal/views/admin/dashboardAdmin.php');
+                break;
+            case 2:
+                header('Location: /Portal/views/gerente/dashboardGerente.php');
+                break;
+            case 0:
+                header('Location: /Portal/views/user/dashboardUser.php');
+                break;
+            default:
+                $_SESSION['error'] = 'Acceso Denegado: Usuario desconocido.';
+                header('Location: index.php?action=showLoginForm');
+                break;
+        }
+        exit;
+    }
 }
-?>

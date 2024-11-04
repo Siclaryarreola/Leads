2d4628aab726f174ffa85fbfbc2ee5e10c2b8c88
Siclary Arreola <siclaryarreola04@gmail.com>
2024-11-04 17:50:53 -0600

    Lead mejora

--- views/gerente/GerenteLeads.php
+++ views/gerente/GerenteLeads.php
@@ -1,39 +1,22 @@
 <?php include('components/header.php'); ?>
 
-<!-- Incluir CSS de DataTables -->
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
+<!-- Incluir CSS de DataTables y Bootstrap -->
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
 
-<!-- Incluir jQuery -->
-<script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<!-- Incluir JS de DataTables -->
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
+<!-- Incluir jQuery y DataTables -->
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js"></script>
+
+<!-- Inicialización de DataTables -->
 <script>
 $(document).ready(function() {
-    // Inicialización de la DataTable
     $('#leadsTable').DataTable({
         "language": {
-            "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json"
+            "url": "../../public/js/Spanish.json" 
         },
-        "initComplete": function() {
-            this.api().columns().every(function() {
-                var column = this;
-                var select = $('<select><option value=""></option></select>')
-                    .appendTo($(column.header()).empty())
-                    .on('change', function() {
-                        var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
-                        );
-
-                        column
-                            .search(val ? '^' + val + '$' : '', true, false)
-                            .draw();
-                    });
-
-                column.data().unique().sort().each(function(d, j) {
-                    select.append('<option value="' + d + '">' + d + '</option>')
-                });
-            });
-        }
+        "order": [[0, "asc"]]
     });
 });
 </script>
@@ -41,7 +24,7 @@ $(document).ready(function() {
 <main class="container-fluid mt-5">
     <h1>Leads</h1>
     <div class="mb-3">
-        <button class="btn btn-primary" onclick="window.location.href='addLead.php'">Agregar Nuevo Lead</button>
+        <button class="btn btn-primary" data-toggle="modal" data-target="#addLeadModal">Agregar nuevo lead</button>
     </div>
 
     <!-- Tabla de Leads -->
@@ -63,26 +46,73 @@ $(document).ready(function() {
             </tr>
         </thead>
         <tbody>
-            <?php if (!empty($leads)): ?>
-                <?php foreach ($leads as $lead): ?>
-                    <tr>
-                        <td><?php echo $lead['empresa']; ?></td>
-                        <td><?php echo $lead['localidad']; ?></td>
-                        <!-- Otros campos... -->
-                        <td>
-                            <a href="viewLead.php?id=<?php echo $lead['id']; ?>" class="btn btn-info btn-sm">Detalle</a>
-                            <a href="editLead.php?id=<?php echo $lead['id']; ?>" class="btn btn-warning btn-sm">Editar</a>
-                            <a href="deleteLead.php?id=<?php echo $lead['id']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('¿Seguro que desea eliminar?');">Eliminar</a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            <?php else: ?>
-                <tr>
-                    <td colspan="12" class="text-center">No hay leads disponibles.</td>
-                </tr>
-            <?php endif; ?>
+            <!-- Datos dinámicos de los leads -->
         </tbody>
     </table>
 </main>
+
+<!-- Modal para Agregar Nuevo Lead -->
+<div class="modal fade" id="addLeadModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalLabel">Agregar nuevo lead</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="addLead.php" method="POST">
+                    <!-- Formulario para agregar lead -->
+                    <div class="form-group">
+                        <label for="empresa">Empresa</label>
+                        <input type="text" class="form-control" id="empresa" name="empresa" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="localidad">Localidad</label>
+                        <input type="text" class="form-control" id="localidad" name="localidad" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="giro">Giro</label>
+                        <input type="text" class="form-control" id="giro" name="giro" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="estado">Estado</label>
+                        <input type="text" class="form-control" id="estado" name="estado" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="contacto">Contacto</label>
+                        <input type="text" class="form-control" id="contacto" name="contacto" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="telefono">Teléfono</label>
+                        <input type="text" class="form-control" id="telefono" name="telefono" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="correo">Correo</label>
+                        <input type="email" class="form-control" id="correo" name="correo" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="fechaProspeccion">Fecha de Prospección</label>
+                        <input type="date" class="form-control" id="fechaProspeccion" name="fechaProspeccion" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="cotizacion">Cotización</label>
+                        <textarea class="form-control" id="cotizacion" name="cotizacion" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="notas">Notas</label>
+                        <textarea class="form-control" id="notas" name="notas" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="archivo">Archivo</label>
+                        <input type="file" class="form-control" id="archivo" name="archivo">
+                    </div>
+                    <button type="submit" class="btn btn-success">Guardar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include('components/footer.php'); ?>
